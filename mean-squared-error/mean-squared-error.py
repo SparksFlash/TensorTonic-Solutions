4df24671093cc14c364941078a699754ec1bbba0
Sandipto Saha <133910106+SparksFlash@users.noreply.github.com>
2026-08-04 12:40:07 +0600

    Solve mean-squared-error

--- mean-squared-error/mean-squared-error.py
+++ mean-squared-error/mean-squared-error.py
@@ -3,4 +3,5 @@ import numpy as np
 def mean_squared_error(y_pred, y_true):
     y = np.array(y_true)
     y_hat = np.array(y_pred)
-    return sum((y - y_hat) ** 2) / len(y)
+    #return sum((y - y_hat) ** 2) / len(y)
+    return np.mean((y - y_hat) ** 2)
